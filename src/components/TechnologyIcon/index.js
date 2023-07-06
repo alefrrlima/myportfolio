3aa16d319ea1665c75539e6cc25ad7c0762d08db
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-07-06 12:50:37 -0300

    Sobre page and components updated

---
+++ src/components/TechnologyIcon/index.js
@@ -0,0 +1,5 @@
+import './TechnologyIcon.styles.css';
+
+export default function TechnologyIcon(props) {
+   return <img className="technologyIcon" src={props.src} alt={props.alt} />;
+}
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,10 +1,41 @@
 import './Sobre.styles.css';
 import Section from '../../components/Section';
+import TechnologyIcon from '../../components/TechnologyIcon';
+
+import html from '../../images/html-icon.png';
+import css from '../../images/css-icon.png';
+import javascript from '../../images/javascript-icon.png';
+import react from '../../images/react-icon.png';
+import figma from '../../images/figma-icon.png';
+import github from '../../images/github-icon.png';
 
 export default function Sobre() {
    return (
       <Section>
-         <div className="sobre">Sobre</div>
+         <div className="sobre">
+            <div>
+               <h4>Olá, me chamo</h4>
+               <span>Alexandre Ferreira Lima</span>
+               <span>Eu idealizo e desenvolvo aplicações para a web.</span>
+            </div>
+            <div>
+               <h1>Desenvolvedor Front-End</h1>
+               <h2>Florianópolis - SC</h2>
+               <h2>24 anos</h2>
+            </div>
+            <div>
+               <h4>Tecnologias utilizadas:</h4>
+               <div>
+                  <TechnologyIcon src={html} alt="html" />
+                  <TechnologyIcon src={css} alt="css" />
+                  <TechnologyIcon src={javascript} alt="javascript" />
+                  <TechnologyIcon src={react} alt="react" />
+                  <TechnologyIcon src={figma} alt="figma" />
+                  <TechnologyIcon src={github} alt="github" />
+               </div>
+            </div>
+            <button>Meu Currículo</button>
+         </div>
       </Section>
    );
 }
