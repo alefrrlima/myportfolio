f21be27bf73573c3193712a4d7023c809e5f46be
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-07-10 09:35:24 -0300

    Contato page updated

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,10 +1,20 @@
 import './Contato.styles.css';
+
+import profilepic from '../../images/profilepic.png'
+
 import Section from '../../components/Section';
+
+
 
 export default function Contato() {
    return (
       <Section>
-         <div className="contato">Contato</div>
+         <img src={profilepic} alt='Minha foto de perfil'/>
+         <div>
+            <div>
+               
+            </div>
+         </div>
       </Section>
    );
 }
