b8188f15ecfbe177ec0707c5e167a58fe45fb021
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-07-11 19:48:50 -0300

    Updating Nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,45 @@
+import { useState } from 'react';
 import './Nav.styles.css';
 
 import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
+   const [currentPage, setCurrentPage] = useState('');
+
    const navigate = useNavigate();
 
    const handleRoute = (path) => {
+      console.log(currentPage);
       navigate(path);
+      setCurrentPage(path);
    };
 
    return (
       <nav className="nav">
-         <button onClick={() => handleRoute('/')}>Sobre</button>
-         <button onClick={() => handleRoute('/projetos')}>Projetos</button>
-         <button onClick={() => handleRoute('/experiencia')}>Experiência</button>
-         <button onClick={() => handleRoute('/contato')}>Contato</button>
+         <button
+            className={currentPage == '/' ? 'active' : 'inactive'}
+            onClick={() => handleRoute('/')}
+         >
+            Sobre
+         </button>
+         <button
+            className={currentPage == '/projetos' ? 'active' : 'inactive'}
+            onClick={() => handleRoute('/projetos')}
+         >
+            Projetos
+         </button>
+         <button
+            className={currentPage == '/experiencia' ? 'active' : 'inactive'}
+            onClick={() => handleRoute('/experiencia')}
+         >
+            Experiência
+         </button>
+         <button
+            className={currentPage == '/contato' ? 'active' : 'inactive'}
+            onClick={() => handleRoute('/contato')}
+         >
+            Contato
+         </button>
       </nav>
    );
 }
