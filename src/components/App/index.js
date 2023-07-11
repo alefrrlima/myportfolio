d3c6cb0df2a43e6822b8db86dbf82f8d9182d98c
Alexandre F. Lima <alexandrelima.beta@gmail.com>
2023-07-11 18:29:33 -0300

    Updating responsive display

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,39 +6,26 @@ import Nav from '../Nav';
 import { BrowserRouter } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-// export default function App() {
-//    const [displayMobile, setDisplayMobile] = useState(
-//       window.innerWidth <= 1000
-//    );
-
-//    useEffect(() => {
-//       const handleResize = () => {
-//          setDisplayMobile(window.innerWidth <= 1000);
-//       };
-
-//       window.addEventListener('resize', handleResize);
-
-//       return () => {
-//          window.removeEventListener('resize', handleResize);
-//       };
-//    }, []);
-
-//    return (
-//       <BrowserRouter>
-//          <div className="app">
-//             {!displayMobile && <Nav />}
-//             <AppRoutes stacked={displayMobile} />
-//          </div>
-//       </BrowserRouter>
-//    );
-// }
-
 export default function App() {
+   const [displayMobile, setDisplayMobile] = useState(window.innerWidth <= 550);
+
+   useEffect(() => {
+      const handleResize = () => {
+         setDisplayMobile(window.innerWidth <= 550);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+         window.removeEventListener('resize', handleResize);
+      };
+   }, []);
+
    return (
       <BrowserRouter>
          <div className="app">
-            <Nav />
-            <AppRoutes />
+            {!displayMobile && <Nav />}
+            <AppRoutes stacked={displayMobile} />
          </div>
       </BrowserRouter>
    );
